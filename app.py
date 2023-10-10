fa51d569d9e36568d4597749252845edb3b77a4d
Chris Roat <1053153+chrisroat@users.noreply.github.com>
2023-10-10 07:18:36 -0700

    Add astronomy cast, drop images and carousel

--- app.py
+++ app.py
@@ -8,25 +8,25 @@ bootstrap = Bootstrap5(app)
 
 @app.route("/")
 def index():
-    fmt = request.args.get("format", "carousel")
-    if fmt not in ["carousel", "list"]:
-        return abort(404)
-    return render_template(f"{ fmt }.html", data=DATA)
+    return render_template(f"index.html", data=DATA)
 
 
 DATA = {
     "podcast": [
         {
+            "title": "Astronomy Cast",
+            "homepage": "https://www.astronomycast.com/",
+            "signup": "https://www.patreon.com/astronomycast",
+        },
+        {
             "title": "Freakonomics",
             "homepage": "https://freakonomics.com",
             "signup": "https://freakonomics.com/plus/",
-            "img": "https://freakonomics.com/wp-content/themes/freakonomics_2021/images/plus_logo_2.png",
         },
         {
             "title": "Mindscape",
             "homepage": "https://www.preposterousuniverse.com/podcast/",
             "signup": "https://www.patreon.com/seanmcarroll",
-            "img": "https://www.preposterousuniverse.com/wp-content/uploads/SCM-rectangle-medium-1.jpg",
         },
     ],
     "news": [
@@ -34,13 +34,11 @@ DATA = {
             "title": "The Guardian",
             "homepage": "https://www.theguardian.com/us",
             "signup": "https://support.theguardian.com/us/contribute",
-            "img": "https://pbs.twimg.com/profile_banners/788524/1542015319/1500x500",
         },
         {
             "title": "San Jose Mercury News",
             "homepage": "https://www.mercurynews.com/",
             "signup": "https://checkout.mercurynews.com/subscriptionmultiproductpw",
-            "img": "https://s3.amazonaws.com/cms.mngcep/TMN_logo_620x76-01.png",
         },
     ],
 }

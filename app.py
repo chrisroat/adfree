5a33fefffded3932ccb59d3af679bbce2a206285
Chris Roat <1053153+chrisroat@users.noreply.github.com>
2023-11-08 17:31:06 -0800

    Add universe today, reformat data

--- app.py
+++ app.py
@@ -8,37 +8,50 @@ bootstrap = Bootstrap5(app)
 
 @app.route("/")
 def index():
-    return render_template(f"index.html", data=DATA)
+    return render_template("index.html", data=DATA)
 
 
-DATA = {
-    "podcast": [
-        {
-            "title": "Astronomy Cast",
-            "homepage": "https://www.astronomycast.com/",
-            "signup": "https://www.patreon.com/astronomycast",
-        },
-        {
-            "title": "Freakonomics",
-            "homepage": "https://freakonomics.com",
-            "signup": "https://freakonomics.com/plus/",
-        },
-        {
-            "title": "Mindscape",
-            "homepage": "https://www.preposterousuniverse.com/podcast/",
-            "signup": "https://www.patreon.com/seanmcarroll",
-        },
-    ],
-    "news": [
-        {
-            "title": "The Guardian",
-            "homepage": "https://www.theguardian.com/us",
-            "signup": "https://support.theguardian.com/us/contribute",
-        },
-        {
-            "title": "San Jose Mercury News",
-            "homepage": "https://www.mercurynews.com/",
-            "signup": "https://checkout.mercurynews.com/subscriptionmultiproductpw",
-        },
-    ],
-}
+DATA = [
+    {
+        "kind": "podcast",
+        "subject": ["astronomy"],
+        "title": "Astronomy Cast",
+        "homepage": "https://www.astronomycast.com/",
+        "signup": "https://www.patreon.com/astronomycast",
+    },
+    {
+        "kind": "podcast",
+        "subject": ["astronomy"],
+        "title": "Universe Today",
+        "homepage": "https://www.universetoday.com/",
+        "signup": "https://www.patreon.com/universetoday",
+    },
+    {
+        "kind": "podcast",
+        "subject": ["economics"],
+        "title": "Freakonomics",
+        "homepage": "https://freakonomics.com",
+        "signup": "https://freakonomics.com/plus/",
+    },
+    {
+        "kind": "podcast",
+        "subject": ["cosmology", "physics", "philosophy"],
+        "title": "Mindscape",
+        "homepage": "https://www.preposterousuniverse.com/podcast/",
+        "signup": "https://www.patreon.com/seanmcarroll",
+    },
+    {
+        "kind": "news",
+        "subject": ["global news"],
+        "title": "The Guardian",
+        "homepage": "https://www.theguardian.com/us",
+        "signup": "https://support.theguardian.com/us/contribute",
+    },
+    {
+        "kind": "news",
+        "subject": ["regional news"],
+        "title": "San Jose Mercury News",
+        "homepage": "https://www.mercurynews.com/",
+        "signup": "https://checkout.mercurynews.com/subscriptionmultiproductpw",
+    },
+]

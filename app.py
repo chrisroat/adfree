bf832461f6a866fb04c6616d3a891b528b54db5f
Chris Roat <1053153+chrisroat@users.noreply.github.com>
2023-11-14 22:12:01 -0800

    Add more data, update formatting

--- app.py
+++ app.py
@@ -1,57 +1,13 @@
-from flask import Flask, abort, render_template, request
+from flask import Flask, render_template
 from flask_bootstrap import Bootstrap5
 
-app = Flask(__name__)
+import data_lib
 
+app = Flask(__name__)
 bootstrap = Bootstrap5(app)
+data = data_lib.load()
 
 
 @app.route("/")
 def index():
-    return render_template("index.html", data=DATA)
-
-
-DATA = [
-    {
-        "kind": "podcast",
-        "subject": ["astronomy"],
-        "title": "Astronomy Cast",
-        "homepage": "https://www.astronomycast.com/",
-        "signup": "https://www.patreon.com/astronomycast",
-    },
-    {
-        "kind": "podcast",
-        "subject": ["astronomy"],
-        "title": "Universe Today",
-        "homepage": "https://www.universetoday.com/",
-        "signup": "https://www.patreon.com/universetoday",
-    },
-    {
-        "kind": "podcast",
-        "subject": ["economics"],
-        "title": "Freakonomics",
-        "homepage": "https://freakonomics.com",
-        "signup": "https://freakonomics.com/plus/",
-    },
-    {
-        "kind": "podcast",
-        "subject": ["cosmology", "physics", "philosophy"],
-        "title": "Mindscape",
-        "homepage": "https://www.preposterousuniverse.com/podcast/",
-        "signup": "https://www.patreon.com/seanmcarroll",
-    },
-    {
-        "kind": "news",
-        "subject": ["global news"],
-        "title": "The Guardian",
-        "homepage": "https://www.theguardian.com/us",
-        "signup": "https://support.theguardian.com/us/contribute",
-    },
-    {
-        "kind": "news",
-        "subject": ["regional news"],
-        "title": "San Jose Mercury News",
-        "homepage": "https://www.mercurynews.com/",
-        "signup": "https://checkout.mercurynews.com/subscriptionmultiproductpw",
-    },
-]
+    return render_template("index.html", data=data)
---
+++ data_lib.py
@@ -0,0 +1,60 @@
+import math
+from collections import defaultdict
+from csv import DictReader
+from pathlib import Path
+
+from us import states
+
+
+def load():
+    raw = read_raw()
+    data = raw_to_dict(raw)
+    for type_, type_data in data.items():
+        data[type_] = make_columns(type_data)
+    return data
+
+
+def read_raw():
+    path = Path(__file__).resolve().parent / "adfree.csv"
+    with path.open() as fp:
+        reader = DictReader(fp)
+        return [row for row in reader if row["has_adfree"] == "yes"]
+
+
+def raw_to_dict(raw):
+    """Translate and convert raw data into nested dict."""
+    data = defaultdict(lambda: defaultdict(list))
+    for item in raw:
+        translate_item(item)
+        data[item["type"]][item["subtype"]].append(item)
+    return data
+
+
+def translate_item(item):
+    if item["type"] == "news":
+        state = states.lookup(item["subtype"])
+        if state:
+            item["subtype"] = state.name
+
+
+def make_columns(type_data, num_columns=3):
+    """Sorts and divides items into columns."""
+    total = sum(len(v) for v in type_data.values()) + 2.1 * len(type_data)
+    col_size = math.ceil(total / num_columns)
+    cols = [[]]
+    count = 1  # Headings take up space
+    for subtype in sorted(type_data):
+        items = type_data[subtype]
+        if count > 1:
+            count += 2  # Headings mid-column take up extra space
+        for item in sorted(items, key=lambda x: remove_the(x["name"])):
+            if count >= col_size:
+                cols.append([])
+                count = 1
+            cols[-1].append(item)
+            count += 1
+    return cols
+
+
+def remove_the(name):
+    return name[4:] if name.startswith("The ") else name
